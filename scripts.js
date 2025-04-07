306c2da6a75be1464d4ad8d5e3443edff620b2b0
brolge <itxbrolge@aol.com>
2025-04-07 21:00:43 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    // the atmospheric layers
+    var troposphere = L.circle([0, 0], {
+        color: 'blue',
+        fillColor: '#30f',
+        fillOpacity: 0.2,
+        radius: 1000000 // 1,000 km
+    }).addTo(map).bindPopup("Troposphere");
+
+    var stratosphere = L.circle([0, 0], {
+        color: 'green',
+        fillColor: '#3f0',
+        fillOpacity: 0.2,
+        radius: 2000000 // 2,000 km
+    }).addTo(map).bindPopup("Stratosphere");
+
     // Fetch and add asteroids data
     fetch('/data/asteroids')
         .then(response => response.json())
@@ -16,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
 
-    // Fetch and add satellites data
+    // Fetch and get the satellite data
     fetch('/data/satellites')
         .then(response => response.json())
         .then(data => {
